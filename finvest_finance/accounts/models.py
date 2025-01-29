4f4da4d607a12de5745bd87757a898eb84848d50
humannx2 <adityavaish0303@gmail.com>
2025-01-29 13:51:46 +0530

    Updated models

--- finvest_finance/accounts/models.py
+++ finvest_finance/accounts/models.py
@@ -10,18 +10,35 @@ class Portfolio(models.Model):
     stock = models.ForeignKey(StockData, on_delete=models.CASCADE, related_name='portfolios')  # ForeignKey to StockData
     quantity = models.PositiveIntegerField()  # Quantity of the stock owned
     purchase_price = models.DecimalField(max_digits=10, decimal_places=2)  # Price at which the stock was purchased
-    date_of_purchase = models.DateField()  # Date of purchase
+    date_of_purchase = models.DateField(blank=True)  # Date of purchase
 
     def __str__(self):
         return f"{self.stock_name.stock_name} - {self.user.username}"  # Display stock name and username
 
 
 
+class Transactions(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='transactions')  # Referenc
+    stock = models.ForeignKey(StockData, on_delete=models.CASCADE, related_name='transactions')
+    quantity = models.PositiveIntegerField()  # Quantity of the stock bought or sold
+    purchase_price = models.DecimalField(max_digits=10, decimal_places=2)  # Price at
+    date_of_transaction = models.DateField(blank=True)  # Date of transaction
+    transaction_type = models.CharField(max_length=4, choices=[('buy', 'sell')])
+    payment_id = models.CharField(max_length=64)
+    payment_screenshot = models.URLField() 
+    
+
 class CustomUser(AbstractUser):
+    name=models.CharField(max_length=32, default="Enter your name")
+    email=models.EmailField(unique=True, default="Enter your email")
+    dob=models.DateField(null=True,blank=True)
     is_verified = models.BooleanField(default=False)  # Whether the user is verified
     demat_account = models.CharField(max_length=20, unique=True, blank=False)  # Demat Account Number
     pan_card_provided = models.BooleanField(default=False)  # If PAN card is uploaded
-    pan_card_number = models.CharField(max_length=10, unique=True, blank=True, null=True)  # Optional PAN card number
+    pan_card_number = models.CharField(max_length=10, unique=True, blank=True, null=True)
+    pan_card_image = models.URLField(default="https://miro.medium.com/v2/resize:fit:1358/1*xMy-08e9N2DuFlhxuWO_sw.jpeg")
+    aadhar_card_image = models.URLField(default="https://1.bp.blogspot.com/-jwvQnOCuftw/Xx2KtXfa-NI/AAAAAAAAECs/kJF4B2rSx8ED3qF9g4Puhf7EDL3p6t_5wCLcBGAsYHQ/s2048/aadhar.png")
+    bank_statement_image = models.URLField(default="https://admeonline.com/wp-content/uploads/2018/07/How-to-get-SBI-account-statement_online-using-internet-banking.png")
 
 
     # Specify related_name to avoid reverse accessor clashes
