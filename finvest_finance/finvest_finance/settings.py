82cefb894481af0e22141199459952431be26548
dev-code0101 <dev.codeone@proton.me>
2025-01-30 14:55:30 +0530

    DEV: setup initial migrations

--- finvest_finance/finvest_finance/settings.py
+++ finvest_finance/finvest_finance/settings.py
@@ -20,7 +20,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure--_o_qk@@zc*3!l)w-1r(c323d$27t&63$#$@grodog#%cs+t@f'
+SECRET_KEY = (
+    'django-insecure--_o_qk@@zc*3!l)w-1r(c323d$27t&63$#$@grodog#%cs+t@f'
+)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
